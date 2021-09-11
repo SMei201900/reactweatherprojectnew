8f490f0e7bc5e7adb9507d3d7dc143402e1fa608
SMei201900 <smei201900@gmail.com>
2021-09-10 23:09:28 -0400

    Created two files called Search and Credits
    As of this commit, they hold nonsense code.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import "./App.css";
+import Search from "./Search.js";
+import Credits from "./Credits";
 
 export default function App() {
 	return (
 		<div className="App">
-			<h1> Hello world</h1>
+			<div className="gradient">
+				<h1>Weather App</h1>
+				<br />
+				<Search />
+				<br />
+				<Credits />
+			</div>
 		</div>
 	);
 }
---
+++ src/Credits.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Credits() {
+	return (
+		<div>
+			<div>here is my name</div>
+			<p>here is the links</p>
+		</div>
+	);
+}
