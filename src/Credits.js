58992f17b57def8c623249afdbf3acc1f21dc68f
SMei201900 <smei201900@gmail.com>
2021-09-10 23:13:34 -0400

    Added Credits

--- src/Credits.js
+++ src/Credits.js
@@ -3,8 +3,34 @@ import React from "react";
 export default function Credits() {
 	return (
 		<div>
-			<div>here is my name</div>
-			<p>here is the links</p>
+			<span className="credit">
+				<a
+					className="codeSource"
+					href="https://github.com/SMei201900/reactweatherprojectnew"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Open source code
+				</a>{" "}
+				by:{" "}
+				<a
+					className="profile"
+					href="https://www.linkedin.com/in/sally-mei-1a4439210/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Sally Mei
+				</a>{" "}
+				and{" "}
+				<a
+					className="hostSite"
+					href="https://condescending-swartz-356e87.netlify.app/"
+					target="_blank"
+					rel="noreferrer"
+				>
+					hosted by Netlify
+				</a>
+			</span>
 		</div>
 	);
 }
